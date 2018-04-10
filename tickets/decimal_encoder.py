4fc0626b171e2c829253705a3ef8168be1ae7e1d
Sirajus Salekin <ssalek14@earlham.edu>
2018-04-10 15:45:27 -0400

    modified:   decimal_encoder.py 	modified:   get.py 	modified:   serverless.yml 	modified:   ../README.md 	modified:   ../instructions.md

--- tickets/decimal_encoder.py
+++ tickets/decimal_encoder.py
@@ -2,10 +2,8 @@ import decimal
 import json
 
 class DecimalEncoder(json.JSONEncoder):
-    """This is a hack around for JSONencoder not being able to encode decimals.
+    """This is a hack around for JSONencoder not being able to encode decimals."""
 
-    found here: https://github.com/serverless/examples/blob/master/aws-python-rest-api-with-dynamodb/todos/decimalencoder.py
-    """
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):
             return int(obj)
--- tickets/get.py
+++ tickets/get.py
@@ -9,16 +9,22 @@ dynamodb = boto3.resource('dynamodb')
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-    desired_ticket = table.get_item(
-        Key = {
-            'id': event['pathParameters']['id']
+    try:
+        desired_ticket = table.get_item(
+            Key = {
+                'id': event['pathParameters']['id']
+            }
+
+        )
+        response = {
+            'statusCode': 200,
+            'body': json.dumps(desired_ticket['Item'], cls=DecimalEncoder)
         }
 
-    )
-
-    response = {
-        "statusCode": 200,
-        'body': json.dumps(desired_ticket['Item'], cls=DecimalEncoder)
-    }
-
+    except KeyError:
+        return {
+            'statusCode':404,
+            'body':json.dumps({"Error Message":"Item specified not found"})
+        }
+    
     return response
